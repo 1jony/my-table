932fef2ee04937432db0fdbd2c39100c609ce335
unknown <jonathanalexiscastanedaaviles@gmail.com>
2025-01-09 16:14:19 -0700

    fix  primer commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,8 @@
 import type { Config } from "tailwindcss";
-
+const { nextui } = require ( "@nextui-org/react" ) ; 
 export default {
   content: [
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}" ,
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,5 +15,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 } satisfies Config;
